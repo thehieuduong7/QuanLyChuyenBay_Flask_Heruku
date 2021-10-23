7fdb90ddb1c1097e65ba0e76d52aa27dff4236b0
Nguyễn Anh Tuấn <79981081+NAT-zz@users.noreply.github.com>
2021-10-23 21:34:30 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -146,7 +146,7 @@ class NguoiDungModelView(AuthenticatedView):
             ('N', 'Nhân Viên Bán Vé'),
             ('K', 'Khách Hàng')
         ]}
-
+#lolhaha
 admin.add_view(KhachHangModelView(KhachHang, db.session, name = "KhachHang"))
 admin.add_view(SanBayModelView(SanBay, db.session, name = "SanBay"))
 admin.add_view(MayBayModelView(MayBay, db.session, name = "MayBay"))
