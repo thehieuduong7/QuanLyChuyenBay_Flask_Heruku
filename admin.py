68df15e2454c4e8da925e170b51193094ab65418
NAT-zz <olivertuan1310@gmail.com>
2021-10-23 21:35:05 +0700

    first commit

--- admin.py
+++ admin.py
@@ -146,7 +146,8 @@ class NguoiDungModelView(AuthenticatedView):
             ('N', 'Nhân Viên Bán Vé'),
             ('K', 'Khách Hàng')
         ]}
-
+#siduiagsd
+#sdsjgsdhgsjdf
 admin.add_view(KhachHangModelView(KhachHang, db.session, name = "KhachHang"))
 admin.add_view(SanBayModelView(SanBay, db.session, name = "SanBay"))
 admin.add_view(MayBayModelView(MayBay, db.session, name = "MayBay"))
