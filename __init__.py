5f329d468fa48a4294df8c904fd1fdc5e5f873c3
lancer-to-learn <docaominhchik17.125@gmail.com>
2021-11-25 23:03:55 +0700

    Load chuyen bay

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:27072000@localhost/finalproj?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 # Cần có key để thao tác với session
 app.secret_key = "AG(ASDAGIA(*&!@"
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from math import prod
+from datetime import datetime
+
+from sqlalchemy.sql.sqltypes import Date
 from models import*
 from __init__ import app, db
 from flask_login import current_user
@@ -22,3 +25,33 @@ def add_user(tendangnhap, tennguoidung, matkhau):
 
 def get_quydinh():
     return QuyDinh.query.all()
+
+def get_new_flight():
+     return ChuyenBay.query.filter(ChuyenBay.ThoiGianXuatPhat.__gt__(datetime.now())).all()
+
+def get_flight_by_id(fid):
+    return ChuyenBay.query.get(fid)
+
+def get_flight(noi_di=None, noi_den=None, time=None):
+    flights = ChuyenBay.query
+    
+    
+    
+    if noi_di and noi_den and time:
+
+        san_bay_di = SanBay.query.filter(DiaChi=noi_di).first()
+        san_bay_den = SanBay.query.filter(DiaChi=noi_den).first()
+        time = datetime.date(time)
+        date = ChuyenBay.ThoiGianXuatPhat.date()
+        flights = ChuyenBay.query.filter(Id_SanBay_Di=san_bay_di.id, 
+                                        Id_SanBay_Den=san_bay_den.id,
+                                        date=time).all()
+    
+    return flights
+
+
+def get_newest_flight():
+    return ChuyenBay.query.filter(ChuyenBay.ThoiGianXuatPhat.__gt__(datetime.now())).order_by(ChuyenBay.ThoiGianXuatPhat).limit(1)
+
+def get_all_san_bay():
+    return SanBay.query.all()
