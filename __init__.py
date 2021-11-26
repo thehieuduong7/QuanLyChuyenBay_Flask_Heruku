8a0da4a81332b8985e0219fb0b4c23bb5dd74a5e
lancer-to-learn <docaominhchik17.125@gmail.com>
2021-11-26 14:24:10 +0700

    Sua

--- __init__.py
+++ __init__.py
@@ -5,13 +5,16 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:27072000@localhost/finalproj?charset=utf8mb4"
+
+#Tuan's
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
 # Cần có key để thao tác với session
 app.secret_key = "AG(ASDAGIA(*&!@"
 
 #đặt tên j cx dc: PAGE_SIZE
-#app.config["PAGE_SIZE"] = 3
+app.config["PAGE_SIZE"] = 1
 
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name = "MY SHOP", template_mode = 'bootstrap4')
