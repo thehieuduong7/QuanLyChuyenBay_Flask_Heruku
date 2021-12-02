26f044d559e56f2b4d0143e7437bed5a1a2290b2
Hiếu Dương <hiudng@outlook.com.vn>
2021-12-02 19:16:27 +0700

    update banVe img

--- ControllerKhachHang.py
+++ ControllerKhachHang.py
@@ -1,11 +1,29 @@
+from sqlalchemy import exc
 from __init__ import db,app
 from models import *
-
+import os
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 class KhachHangController():
-    def insert(self,kh):
+    def insert(self,data):
+        HoTenKH=data['HoTenKH']
+        GioiTinh=data['GioiTinh']
+        NamSinh=data['NamSinh']
+        SDT=data['SDT']
+        CMND=data['CMND']
+        Email=data['Email']
+        HinhAnh = data['HinhAnh']
+        kh = KhachHang(HoTenKH=HoTenKH,GioiTinh=GioiTinh,
+                       NamSinh=NamSinh,SDT=SDT,
+                       CMND=CMND,Email=Email)
         db.session.add(kh)
-        kh = KhachHang.query.order_by(KhachHang.id.desc()).first()
-        return kh    
+        db.session.flush()
+        db.session.refresh(kh)
+        id= kh.id
+        filename= 'hìnhAnh'+str(id)+'.png'
+        HinhAnh=self.saveImg(HinhAnh,filename)
+        kh.HinhAnh=HinhAnh
+        return kh
+        
     
     def delete(self,kh):
         db.session.delete(kh)
@@ -14,4 +32,14 @@ class KhachHangController():
         search= "%"+search.strip()+"%"
         return KhachHang.query.filter(KhachHang.HoTenKH.like(search)).all()
     
-    
+
+
+    def allowed_file(self,filename):
+	    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+ 
+    def saveImg(self,img,filename):
+        if not (img and self.allowed_file(img.filename)):
+            return None
+        path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        img.save(path) 
+        return path
