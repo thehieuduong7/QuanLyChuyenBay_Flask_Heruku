1078f3b32605ab709074a21262716c85061214b3
lancer-to-learn <docaominhchik17.125@gmail.com>
2021-12-03 21:14:58 +0700

    a

--- __init__.py
+++ __init__.py
@@ -5,26 +5,18 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-<<<<<<< HEAD
 
 #Tuan's
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
-=======
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
-
-
-# Tuan's
-# app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
-
-
->>>>>>> 447f85ffd0b7ea425d5bd2c25f0b52ca74bc7c9b
+#app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:tuan@1310@localhost/finalproj?charset=utf8mb4"
+#Chi's
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:27072000@localhost/finalproj?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 # Cần có key để thao tác với session
 app.secret_key = "AG(ASDAGIA(*&!@"
 
 #đặt tên j cx dc: PAGE_SIZE
-app.config["PAGE_SIZE"] = 1
+app.config["PAGE_SIZE"] = 3
 
 db = SQLAlchemy(app=app)
 admin = Admin(app=app, name = "MY SHOP", template_mode = 'bootstrap4')
